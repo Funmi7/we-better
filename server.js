ba0a161de0a568a68f8ec7b3c9ee77f42a0adb0d
funmi7 <talabifunmilayo@gmail.com>
2020-09-04 03:35:43 +0100

    Back end server endpoints working in local

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ require("dotenv/config");
 const server = express();
 
 const authRouter = require('./auth/auth-router');
+const issueRouter = require('./routes/issues-routers');
 
 server.use(cors());
 server.use(bodyParser.json());
 server.use(express.json());
 
 server.use('/auth', authRouter);
+server.use('/issues', issueRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json("Api is working");
